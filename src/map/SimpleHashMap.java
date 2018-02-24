124b0938eead05567d3019a8f4f5dabc7d20ea4d
jonatannilsson <nilsson.jonatan98@gmail.com>
2018-02-24 18:17:27 +0100

    Implement rehash and fix naming for member variables

--- src/map/SimpleHashMap.java
+++ src/map/SimpleHashMap.java
@@ -2,13 +2,13 @@ package map;
 
 public class SimpleHashMap<K,V>  implements Map<K,V> {
 
-    Entry<K, V>[] Table;
-    private int size = 0;
+    private Entry<K, V>[] m_table;
+    private int m_size = 0;
 
     public static class Entry<K,V> implements Map.Entry<K, V> {
         private K m_key;
         private V m_value;
-        private Entry next;
+        private Entry<K,V> m_next;
 
         Entry (K key, V value){
             m_key = key;
@@ -42,22 +42,22 @@ public class SimpleHashMap<K,V>  implements Map<K,V> {
     /** Constructs an empty hashmap with the default initial capacity (16)
      and the default load factor (0.75). */
     public SimpleHashMap(){
-        Table = (Entry<K,V>[]) new Entry[100];
+        m_table = (Entry<K,V>[]) new Entry[100];
     }
     /** Constructs an empty hashmap with the specified initial capacity
      and the default load factor (0.75). */
     public SimpleHashMap(int capacity){
-        Table = (Entry<K,V>[]) new Entry[capacity];
+        m_table = (Entry<K,V>[]) new Entry[capacity];
     }
 
     String show(){
         StringBuilder result = new StringBuilder();
 
-        for (Entry<K, V> aTable : Table) {
+        for (Entry<K, V> aTable : m_table) {
             Entry entry = aTable;
             while (entry != null) {
                 result.append(entry);
-                entry = entry.next;
+                entry = entry.m_next;
             }
             result.append('\n');
         }
@@ -75,34 +75,51 @@ public class SimpleHashMap<K,V>  implements Map<K,V> {
 
     @Override
     public boolean isEmpty() {
-        return size == 0;
+        return m_size == 0;
     }
 
     @Override
     public V put(K key, V value) {
 
+        if (m_size * 3 / 4.0 >= m_table.length) rehash();
+
         int index = index(key);
         Entry<K,V> oldEntry = find(index, key);
 
         if (oldEntry == null){
-            size++;
+            m_size++;
 
-            if (Table[index] == null) {
-                Table[index] = new Entry<>(key, value);
+            if (m_table[index] == null) {
+                m_table[index] = new Entry<>(key, value);
                 return null;
             }
 
-            Entry<K, V> last = Table[index];
-            while (last.next != null){
-                last = last.next;
+            Entry<K, V> last = m_table[index];
+            while (last.m_next != null){
+                last = last.m_next;
             }
-            last.next = new Entry<>(key, value);
+            last.m_next = new Entry<>(key, value);
             return null;
         }
         else{
             V old_value = oldEntry.m_value;
             oldEntry.m_value = value;
             return old_value;
+        }
+    }
+
+    private void rehash(){
+        Entry<K,V>[] oldTable = m_table;
+
+        m_table = (Entry<K,V>[]) new Entry[(m_table.length * 2)];
+        m_size = 0;
+
+        for (Entry<K, V> list : oldTable) {
+            Entry<K, V> entry = list;
+            while (entry != null) {
+                put(entry.m_key, entry.m_value);
+                entry = entry.m_next;
+            }
         }
     }
 
@@ -113,18 +130,18 @@ public class SimpleHashMap<K,V>  implements Map<K,V> {
 
     @Override
     public int size() {
-        return size;
+        return m_size;
     }
 
 
     private int index(K key){
         int hashCode = key.hashCode();
-        return (hashCode < 0 ? -hashCode : hashCode) % Table.length;
+        return (hashCode < 0 ? -hashCode : hashCode) % m_table.length;
     }
 
     private Entry<K,V> find(int index, K key){
-        Entry entry = Table[index];
-        while (entry != null && !entry.m_key.equals(key)) entry = entry.next;
+        Entry<K,V> entry = m_table[index];
+        while (entry != null && !entry.m_key.equals(key)) entry = entry.m_next;
         return entry;
     }
 }
